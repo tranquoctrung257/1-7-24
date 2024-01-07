5ee2f1f05507895402156797417a079f3e6be1eb
tranquoctrung257 <tt3617089@gmail.com>
2024-01-07 12:29:16 +0700

    fist commit

---
+++ 1.py
@@ -0,0 +1,28 @@
+# cách setup vscode
+
+# bật auto save
+# ở góc trái màn hình vào file sau đó chọn auto_save
+
+# các extension cần thiết
+# 1.python
+# 2.codeRunner
+# 3.codeSnap
+# 4.VSCode Great Icons
+
+# cách setting 
+# ctr + ,
+# vào mục extension kiểm mục run code configuration
+# tích vào mục Clear Previous Output # mỗi lần chạy lại nó sẽ xóa terminal cũ
+# bỏ tích Compact Folders
+# bỏ tích Run In Terminal
+# tích vào mục Execute In File Dir
+# tích vào Format On Save
+# vào mục Executor Map rồi cho dòng python như thế này 
+
+# để chụp hình bằng code runner thì bôi đên code rồi ấn ctrl+shift+p hoặc ấn chuột phải chọn code snap
+
+# để di chuyển một dòng nào đó ta ấn phím art + phím xuông hoặc lên
+
+# ctrl shift L để chọn nhiều từ từ 1 từ mình mới bôi đen
+
+# ctrl F để tìm kiếm
