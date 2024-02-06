f6035e4a2cfd95170e610cb4db81dee6d122c981
tranquoctrung257 <tt3617089@gmail.com>
2024-02-06 21:31:08 +0700

    update

---
+++ 2_6_2024/1/2.py
@@ -0,0 +1,18 @@
+# để chạy pycharm cell mode gõ ##
+
+##
+print("xin chào")
+
+# + - * / // ** %
+# ** lũy thừa ví dụ a^n
+# % chia lấy phần dư
+# // chia lấy phần nguyên
+
+# lưu ý: mức độ ưu tiên của ** cao hơn - nên phép toán 1 ** 0 sẽ thực hiện trước rồi mới đến - kết quả đó
+print(-1 ** 0)
+print(-1000 ** 1)
+# .1 = 0.1
+print(.1 + .4)
+
+# > < >= <= == != ==> True/False
+# and or not
