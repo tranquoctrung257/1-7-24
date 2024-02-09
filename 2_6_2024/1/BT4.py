e0c5e18840ffe59e25892e6449517d5e70422ed4
tranquoctrung257 <tt3617089@gmail.com>
2024-02-09 08:10:30 +0700

    update

--- 2_6_2024/1/BT4.py
+++ 2_6_2024/1/BT4.py
@@ -1,3 +1,4 @@
+# Bài 1
 # 1. Tạo một movies list chứa tên các bộ phim đã xem
 
 movies_list = ["trảng quỳnh","Naruto","Fairy Tail","gió","Fairy Tail"]
@@ -45,3 +46,46 @@ print(f"danh sách sau khi thêm là: {movies_list}")
 # 12. Xóa tất cả các bộ phim có trong danh sách
 movies_list.clear()
 print(f"danh sách sau khi xóa là: {movies_list}")
+
+## bài 2
+friends = ["Jen", "Jack", "Kenny", "Jelly", "Bob", "Henry", "Anne"]
+
+#a Lấy ra 4 người bạn đầu tiên trong friends
+print(f"4 người bạn đầu tiên trong friends là: {friends[:4]}")
+
+#b Lấy ra 4 người bạn cuối trong friends
+print(f"4 người bạn cuối trong friends là {friends[3:]}")
+
+#c Đảo ngược danh sách friends
+reversed_lst = friends[::-1]
+print(f"danh sách sau khi Đảo ngược friends là {reversed_lst}")
+
+#e Copy danh sách ban đầu thành một danh sách mới
+lst_copy = friends.copy()
+print(friends is lst_copy)
+print(friends == lst_copy)
+
+#f Lấy ra những người bạn từ vị trí 2 đến sát cuối
+print(f"những người bạn từ vị trí 2 đến sát cuối{friends[2:-1]}")
+
+##
+#            0          1      2
+students = [
+            ["SV001", "Bob", 23],     #    0
+            ["SV002", "Kenny", 34],   #    1
+            ["SV003", "Henry", 45]    #    2
+]
+
+#a Lấy ra thông tin của sinh viên thứ nhất và in ra định dạng "ID: {id}, name: {name} - age: {age}"
+
+print(f"ID: {students[0][0]}, name: {students[0][1]} - age: {students[0][-1]}")
+
+#b Lấy ra tuổi của sinh viên thứ hai
+print(students[1][-1])
+
+#c Lấy ra thông tin hai sinh viên cuối cùng
+print(f"ID: {students[1][0]}, name: {students[1][1]} - age: {students[1][-1]}")
+print(f"ID: {students[-1][0]}, name: {students[-1][1]} - age: {students[-1][-1]}")
+
+#d Lấy ra id của sinh viên thứ ba
+print(f"ID: {students[-1][0]}")
